e2a85f8ffc6cb341c75e27a4d58c9785715bf308
soyona <kanglei@shopin.cn>
2018-04-14 19:44:03 +0800

    Add SampleMvcApplication

---
+++ spring-boot-sample-mvc/src/main/java/sample/mvc/SampleMvcApplication.java
@@ -0,0 +1,11 @@
+package sample.mvc;
+
+/**
+ * @author kanglei
+ * @Title: ${file_name}
+ * @Package sample.mvc
+ * @Description: ${todo}
+ * @date 2018/4/1419:38
+ */
+public class SampleMvcApplication {
+}
